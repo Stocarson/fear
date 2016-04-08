85767b98c9428ec675e193d3d1e34c3bf1773999
Niccolò Izzo <izzo.niccolo@gmail.com>
2016-04-08 17:52:48 +0200

    Added more code for generating tables.

--- sbox.py
+++ sbox.py
@@ -3,8 +3,40 @@
 uppermask = (2**8-1)<<8
 lowermask = (2**8-1)
 
+sbox_input_bits = 16
+sbox_output_bits = 8
+
 def sbox0(input):
     return (((input & uppermask) + (input & lowermask)) % 256 ) << 2
+
+def isRelationTrue(inbits, outbits, sboxinput, sboxoutput):
+    mask = 0
+    inputrelation = 0
+    outputrelation = 0
+    for i in range(sbox_input_bits):
+           mask = 1 << i
+           if inbits & mask != 0:
+               inputrelation ^= (sboxinput & mask) >> i
+    for i in range(sbox_output_bits):
+        mask = 1 << i
+        if outbits & mask != 0:
+            outputrelation ^= (sboxoutput & mask) >> i
+
+    if inputrelation == outputrelation:
+        return 1
+    else:
+        return 0
+
+def buildlineartable():
+    table = [[[] for j in range(sbox_output_bits)] for i in range(sbox_input_bits)]
+    for i in range(sbox_input_bits):
+        for j in range(sbox_output_bits):
+            for z in range(sbox_input_bits):
+                table[i][j] += isRelationTrue(i, j, z, sbox0(z))
+    for i in range(sbox_input_bits):
+        for j in range(sbox_output_bits):
+            table[i][j] -= (sbox_output_bits/2)
+    return table
 
 if __name__ == "__main__":
     output = ""
